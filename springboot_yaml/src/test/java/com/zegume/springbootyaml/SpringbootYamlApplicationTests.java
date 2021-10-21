48fdd50b8034530d29d440fe9b7265597fff7a1b
U-DESKTOP-B2JRV0G\mehmeto <fmozturk@gmail.com>
2021-10-21 17:29:01 +0300

    spring_yaml first commit

---
+++ springboot_yaml/src/test/java/com/zegume/springbootyaml/SpringbootYamlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zegume.springbootyaml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootYamlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
